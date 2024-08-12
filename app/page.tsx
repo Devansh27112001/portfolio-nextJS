56174205ab061b222758daa3a0c1d186b67e8ab5
Devansh27112001 <dkansara2711@gmail.com>
2024-08-12 11:59:37 -0400

    Working on Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
-  return <div>Devansh portfolio in NextJs</div>;
+  return (
+    <main
+      className="relative bg-black-100 text-white flex justify-center items-center flex-col mx-auto
+    overflow-hidden sm:px-10 px-5"
+    >
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
+  );
 }
